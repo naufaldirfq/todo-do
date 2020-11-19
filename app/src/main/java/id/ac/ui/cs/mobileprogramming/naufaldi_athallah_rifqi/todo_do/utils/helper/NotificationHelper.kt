176a4c75bc75b6f4d36cb0f7cc0006a7f2d54810
Naufaldi Athallah Rifqi <naufaldi.rifqi@akseleran.com>
2020-11-19 07:26:29 +0700

    add features:
    - Notification
    - Delete all todos

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/naufaldi_athallah_rifqi/todo_do/utils/helper/NotificationHelper.kt
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.mobileprogramming.naufaldi_athallah_rifqi.todo_do.utils.helper
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+object NotificationHelper {
+    fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String) {
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channelId = "${context.packageName}-$name"
+            val channel = NotificationChannel(channelId, name, importance)
+            channel.description = description
+            channel.setShowBadge(showBadge)
+
+            // Register the channel with the system
+            val notificationManager = context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
